7bebd597b21ca1ddef48634d5d59ef09d1eb2cfd
thisjuizkuf <Izzymendeljnr@gmail.com>
2025-06-24 12:46:00 -0400

    Update price.tsx

--- src/modules/products/components/product-preview/price.tsx
+++ src/modules/products/components/product-preview/price.tsx
@@ -10,20 +10,20 @@ export default async function PreviewPrice({ price }: { price: VariantPrice }) {
     <>
       {price.price_type === "sale" && (
         <Text
-          className="line-through text-ui-fg-muted"
+          className="line-through text-ui-fg-subtle" // Changed from muted to subtle for darker
           data-testid="original-price"
         >
           {price.original_price}
         </Text>
       )}
       <Text
-        className={clx("text-ui-fg-muted", {
+        className={clx("text-ui-fg-base", { // Changed from muted to base for darker
           "text-ui-fg-interactive": price.price_type === "sale",
         })}
         data-testid="price"
       >
         {price.calculated_price}
-      </Text>
+        </Text>
     </>
   )
 }
