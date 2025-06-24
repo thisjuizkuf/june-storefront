97816174933eff97a9b98668c25432ce41e3adc4
thisjuizkuf <Izzymendeljnr@gmail.com>
2025-06-24 14:48:03 -0400

    Update constants.tsx

--- src/lib/constants.tsx
+++ src/lib/constants.tsx
@@ -11,7 +11,7 @@ export const paymentInfoMap: Record<
   { title: string; icon: React.JSX.Element }
 > = {
   pp_stripe_stripe: {
-    title: "Credit card",
+    title: "Credit Or Debit card",
     icon: <CreditCard />,
   },
   "pp_stripe-ideal_stripe": {
